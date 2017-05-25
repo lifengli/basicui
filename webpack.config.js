acaf0153330ae69c566dee91d3a1132f2ca9f63c
Lifeng Li <lifli@cisco.com>
2017-05-25 16:48:54 -0400

    update package and add webpack modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,28 @@
-const webpack = require('webpack');
 const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
 
-const serverEndpoint = process.env.MY_SERVER_ENDPOINT || 'http://0.0.0.0:3000';
+const UIPATH = process.env.UI_PATH;
+const TARGET = process.env.npm_lifecycle_event;
+const BUILD_TYPE = process.env.BUILD_TYPE || 'dev';
+
+const PATHS = {
+  app: path.join(__dirname, './src/Application.jsx'),
+  build: path.join(__dirname, '../uiserver/public/javascripts')
+};
+
 const devHost = process.env.MY_DEV_HOST || '0.0.0.0';
 const devPort = process.env.MY_DEV_PORT || 8080;
+const serverEndpoint = process.env.MY_SERVER_ENDPOINT || 'http://0.0.0.0:3000';
 
-module.exports = {
-  entry: ['babel-polyfill', './src/Application.jsx'],
+const common = {
+  entry: ['babel-polyfill', PATHS.app],
   output: {
+    path: PATHS.build,
     filename: 'bundle.js',
     publicPath: '/'
   },
   cache: true,
-  devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx'],
     alias: {
@@ -39,45 +49,85 @@ module.exports = {
         include: __dirname
       }
     ]
-  },
-  devServer: {
-    contentBase: './dev',
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    stats: 'errors-only',
-    host: devHost,
-    port: devPort,
-    compress: true,
-    disableHostCheck: true,
-    proxy: {
-      '/api/*': {
-        target: serverEndpoint,
-        secure: false
-      },
-      '/stylesheets/*': {
-        target: serverEndpoint,
-        secure: false
-      },
-      '/fonts/*': {
-        target: serverEndpoint,
-        secure: false
-      },
-      '/images/*': {
-        target: serverEndpoint,
-        secure: false
-      }
-    }
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.ProvidePlugin({
-      fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-    }),
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery'
-    })
-  ]
+  }
 };
+
+if (BUILD_TYPE !== 'dev') {
+  common.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        sequences: true,
+        properties: true,
+        dead_code: true,
+        conditionals: true,
+        comparisons: true,
+        loops: true,
+        booleans: true,
+        unused: true,
+        if_return: true,
+        join_vars: true,
+        drop_console: true,
+        drop_debugger: true
+      },
+      comments: false,
+      minimize: true
+    })
+  );
+}
+else {
+  common.devtool = 'source-map';
+}
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: './dev',
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      stats: 'errors-only',
+      host: devHost,
+      port: devPort,
+      compress: true,
+      disableHostCheck: true,
+      proxy: {
+        '/api/*': {
+          target: serverEndpoint,
+          secure: false
+        },
+        '/favicon.ico': {
+          target: serverEndpoint,
+          secure: false
+        },
+        '/fonts/*': {
+          target: serverEndpoint,
+          secure: false
+        },
+        '/images/*': {
+          target: serverEndpoint,
+          secure: false
+        },
+        '/stylesheets/*': {
+          target: serverEndpoint,
+          secure: false
+        }
+      }
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.ProvidePlugin({
+        fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+      }),
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
+      })
+    ]
+  });
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}
